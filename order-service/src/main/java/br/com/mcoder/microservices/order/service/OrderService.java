a0dd1e26000ce677d348b98841599f38ef298ad3
marcelloCoder <deoliveiral767@gmail.com>
2025-01-23 11:34:56 -0300

    Kakfka tracing implements

--- order-service/src/main/java/br/com/mcoder/microservices/order/service/OrderService.java
+++ order-service/src/main/java/br/com/mcoder/microservices/order/service/OrderService.java
@@ -2,12 +2,14 @@ package br.com.mcoder.microservices.order.service;
 
 import br.com.mcoder.microservices.order.client.InventoryClient;
 import br.com.mcoder.microservices.order.dto.OrderRequest;
+import br.com.mcoder.microservices.order.event.OrderPlacedEvent;
 import br.com.mcoder.microservices.order.model.Order;
 import br.com.mcoder.microservices.order.repository.OrderRepository;
 import groovy.util.logging.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -21,6 +23,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
     public void placeOrder(OrderRequest orderRequest){
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
         log.info("Verificando estoque para o código SKU: {}", orderRequest.skuCode());
@@ -35,6 +38,14 @@ public class OrderService {
             order.setQuantity(orderRequest.quantity());
             order.setSkuCode(orderRequest.skuCode());
             orderRepository.save(order);
+
+            //Mandar mensagem para Kafka topico
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+            log.info("Start - Sending OrderPlaced Event {} to kafka topic order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending OrderPlaced Event {} to kafka topic order-placed", orderPlacedEvent);
+
+
         }else {
             throw new RuntimeException("Produto com codigo sku " + orderRequest.skuCode() + " não esta no estoque");
         }
